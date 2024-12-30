f960d54787148898396db58bead4fc008d8e1637
Arafat-Hossain-Anik <arafatskd@gmail.com>
2024-12-30 11:27:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.post("/obdx/api", (req, res) => {
+    console.log("Request received");
+    const data = req.body;
+    console.log(data);
+    const response = {
+        "status": {
+            "result": "success",
+        },
+        "receivedResponseData": "0"
+    };
+    res.send(response);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
